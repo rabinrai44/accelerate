e678ff1021f2913b4c5e8ccb582fe53d85e6eb41
Rabin <rabinrai44@gmail.com>
2017-05-16 14:35:11 +0000

    CSS added to case-study single post

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -9,55 +9,15 @@
 
 get_header(); ?>
 
-	<!-- BLOG PAGE -->
-	<section class="blog-page">
-		<div class="site-content">
-			<div class="main-content">
-				
-				<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post(); ?>
-
-				<article class="post-entry individual-post">
-					<div class="entry-wrap">
-						
-						<header class="entry-header">
-							<div class="entry-meta">
-								<time class="entry-time"><?php the_date();?></time>
-							</div>
-							<h2 class="entry-title"><?php the_title(); ?></h2>
-						</header>
-						
-						<div class="entry-summary">
-							<?php the_content(); ?>
-						</div>
-						
-						<footer class="entry-footer">
-							<div class="entry-meta">
-								<span class="entry-terms author">Written by <?php the_author_posts_link(); ?></span>
-								<span class="entry-terms category">Posted in <?php the_category(', '); ?></span>
-								<span class="entry-terms comments"><?php comments_number( 'No comments yet!', '1 comment', '% comments' ); ?></span>
-							</div>
-						</footer>
-						
-					</div>
-				</article>
-
-					<?php 
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif; ?>
-			</div>
-
-			<?php get_sidebar(); ?>
-
-			<footer class="navigation container">
-				<div class="left">&larr;<a href="<?php echo home_url(); ?>/blog">back to posts</a></div>
-			</footer>
-	 
-				<?php endwhile; ?>
-			</div>
-		</section>	
-		
+	<div id="primary" class="site-content">
+	    <div id="content" role="main">
+	        <?php while ( have_posts() ) : the_post(); ?>
+	        
+	        <h1>This is my case Studies </h1>
+	        
+	        <?php the_content(); ?>
+	        <?php endwhile; // end of the loop ?>
+	    </div><!-- #content -->
+	</div><!-- #primary -->
 <?php
 get_footer();
