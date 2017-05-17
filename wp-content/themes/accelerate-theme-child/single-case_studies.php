5db196cfcda61022497cc8b4a5f85725ee950a95
Rabin <rabinrai44@gmail.com>
2017-05-17 06:58:54 +0000

    removed img tag from echo wp_attachement_image

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -38,16 +38,16 @@ get_header(); ?>
 	        <div class="case-study-images">
 	        	<?php if ($image_1) { ?>
 	            
-	            <img src="<?php echo wp_get_attachment_image($image_1, $size); ?>">
+	            <?php echo wp_get_attachment_image($image_1, $size); ?>
 	            <?php } ?>
 	            	<?php if ($image_2) { ?>
 	            
-	            <img src="<?php echo wp_get_attachment_image($image_2, $size); ?>">
+	            <?php echo wp_get_attachment_image($image_2, $size); ?>
 	            <?php } ?>
 	            
 	            	<?php if ($image_3) { ?>
 	            
-	            <img src="<?php echo wp_get_attachment_image($image_3, $size); ?>">
+	            <?php echo wp_get_attachment_image($image_3, $size); ?>
 	            <?php } ?>
 	            
 	        </div>
