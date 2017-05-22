3f68eda247ea24b490d3619b6cf14af351d79caf
Rabin <rabinrai44@gmail.com>
2017-05-22 17:14:03 +0000

    Added archive-case_studies for individual archive page

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the all case studies page
+ *
+ * 
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
