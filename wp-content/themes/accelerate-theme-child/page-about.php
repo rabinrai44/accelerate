7a3f46a9e297c5084e5c6f7954a04ba3a7979008
Rabin <rabinrai44@gmail.com>
2017-06-01 06:32:03 +0000

    Added section and some divs for about-page layout

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -14,14 +14,45 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+<section class="about-hero">
+	<div class="site-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class='homepage-hero'>
+			    <?php the_content(); ?>
+			</div>
+		<?php endwhile; // end of the loop. ?>
+		
+	</div><!-- .container -->
+</section><!-- .home-page -->
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+<section class="about-page">
+    <div class="site-content">
+        
+        <h2>Our Services</h2>
+        <p><?php the_content; ?></p>
+   
+        
+    </div><!-- .site-content -->
+</section><!-- .featured-work -->
+
+<section class="our-services">
+    <div class="site-content">
+        <h2>Our Services</h2>
+        <p>We take pride in our clients and the content we create for theme. <br> Here's a  brief overview of our offered services. </p>
+        <div class="service">
+        	<div class="service-icon">
+        		
+        	</div><!-- .service-icon -->
+        	<div class="service-content">
+        		
+        	</div><!-- .service-content -->
+        </div><!-- .service -->
+        
+        <div class="accelerate-optin">
+        	<h2>Interested in working with us?</h2> <a href="#"><button class="btn-main">Contact Us</button></a>
+        </div>
+    </div><!-- .site-content -->
+</section><!-- .our-services -->
+
 
 <?php get_footer(); ?>
