cd69053a55e575d3735124f9258b24dc667c05d8
Rabin <rabinrai44@gmail.com>
2017-06-04 07:05:27 +0000

    Styles updated and some removed

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-<section class="about-hero">
+<section class="home-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
